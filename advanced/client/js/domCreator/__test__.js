ac5689f1e78a2bbdd8f82af2ed060bce4dcf80ce
WallyNegima <u.str.gm@gmail.com>
2021-04-26 18:16:34 +0900

    テストかけそうな雰囲気だした

--- advanced/client/js/domCreator/__test__.js
+++ advanced/client/js/domCreator/__test__.js
@@ -1,3 +1,24 @@
+import assert from 'assert';
 import { createTodoItemElList } from "./todoList.js";
 
 // todoListを渡したらテストできる
+describe('appendList', function () {
+    document.body.innerHTML = `<template id="todo-item-template">
+    <li class="todo-item">
+      <label class="todo-toggle__container">
+        <input
+          data-todo-id="-1"
+          type="checkbox"
+          class="todo-toggle"
+          value="checked"
+          />
+        <span class="todo-toggle__checkmark"></span>
+      </label>
+      <div class="todo-name"></div>
+      <div data-todo-id="-1" class="todo-remove-button">x</div>
+    </li>
+  </template>`;
+
+  
+    
+})
--- advanced/client/js/domCreator/todoList.js
+++ advanced/client/js/domCreator/todoList.js
@@ -17,7 +17,31 @@ export const createTodoItem = (todo) => {
     return clone
   } else {
     // HTML template 要素に対応していないので
-    // 表に行を追加するほかの方法を探します。
+    const item = document.createElement("li")
+    item.classList.add("todo-item");
+    const label = document.createElement("label")
+    label.classList.add("todo-toggle__container");
+    const checkBox = document.createElement("input")
+    checkBox.dataset.todoId = todo.id;
+    checkBox.classList.add("todo-toggle");
+    checkBox.value = todo.done;
+    checkBox.checked = todo.done;
+    checkBox.type = "checkbox";
+    const span = document.createElement("span")
+    span.classList.add("todo-toggle__checkmark");
+    label.appendChild(checkBox)
+    label.appendChild(span)
+    item.appendChild(label)
+    const todoNameDiv = document.createElement("div")
+    todoNameDiv.classList.add("todo-name");
+    todoNameDiv.appendChild(document.createTextNode(todo.name ? todo.name : "-"))
+    item.appendChild(todoNameDiv)
+    const removeButton = document.createElement("div")
+    removeButton.classList.add("todo-remove-button");
+    removeButton.appendChild(document.createTextNode("x"))
+    removeButton.dataset.todoId = todo.id;
+    item.appendChild(removeButton)
+    return item
   }
 }
 
